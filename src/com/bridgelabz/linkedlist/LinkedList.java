08f04a075f0e76eb4ed5e214151a1871d0a06d3e
shekhar <shekharjha7417870578@gmail.com>
2022-08-19 22:39:53 +0530

    Added uc4 : Added insertAtmid  method

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -3,41 +3,33 @@ package com.bridgelabz.linkedlist;
 public class LinkedList<T> {
     Node<T> head;
     Node<T> tail;
-    private int size;
+
     public void push(T data) {
         Node<T> newNode = new Node<>(data);
-        if(head == null){
+        if (head == null) {
             head = newNode;
             tail = newNode;
-        }else{
+        } else {
             newNode.next = head;
             head = newNode;
         }
-}
+    }
 
 
 
         public void show() {
-            if (head == null) {
+            if(head == null){
                 System.out.println("Linked list is Empty");
-            } else {
+            }
+            else{
                 Node<T> temp = head;
-                while (temp != null) {
+                while(temp != null){
                     System.out.print(temp.data + " ");
                     temp = temp.next;
                 }
                 System.out.println();
             }}
-            public void add(T data) {
-                    Node<T> newNode = new Node(data);
-                 if (head == null) {
-                     head = newNode;
-                 }else{
-                    tail.next = newNode;
-        }
-        tail = newNode;
-    }
-    public void append ( T data ) {
+    public void add(T data){
         Node<T> newNode = new Node(data);
         if (head == null) {
             head = newNode;
@@ -47,8 +39,49 @@ public class LinkedList<T> {
         tail = newNode;
     }
 
+   // public void append ( T  data ) {
+     //   Node<T> newNode1 = new Node(data);
+       // if (head == null) {
+         //   head = newNode;
+        //}else{
+          //  tail.next = newNode;
+        //}
+        //tail = newNode;
+   public void insertAtMid(T data)
+   {
+       // if list is empty
+       if (head == null)
+           head = new Node(data);
+       else {
 
-    }
+           Node newNode = new Node(data);
+
+           Node ptr = head;
+           int len = 0;
+
+
+           while (ptr != null) {
+               len++;
+               ptr = ptr.next;
+           }
+
+
+           int count = ((len % 2) == 0) ? (len / 2) :
+                   (len + 1) / 2;
+           ptr = head;
+
+
+           while (count-- > 1)
+               ptr = ptr.next;
+
+
+           newNode.next = ptr.next;
+           ptr.next = newNode;
+       }
+   }  }
+
+
+
 
 
 
