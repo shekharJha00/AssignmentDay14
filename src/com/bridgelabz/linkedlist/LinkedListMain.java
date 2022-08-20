fb3266218c20d9e06de97df3ada63495c01ba74f
shekhar <shekharjha7417870578@gmail.com>
2022-08-20 21:01:01 +0530

    Added uc9 : deleting 40 from list and printing list.size()

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.linkedlist;
 
+
 public class LinkedListMain {
     public static void main(String[] args) {
 
@@ -11,7 +12,13 @@ public class LinkedListMain {
         list3.insertAtMid(30);
         list3.show();
         list3.insertAtPos(40,2);
+        list3.search(40);
+        System.out.println("List before deletion");
         list3.show();
+        list3.popAt(2);
+        System.out.println("List after deleting 40 ");
+        list3.show();
+        System.out.println("the size of list is :"+list3.size());
 
 
 
